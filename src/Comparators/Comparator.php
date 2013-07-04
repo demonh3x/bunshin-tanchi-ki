b3014ef2adc0d76fdd344b24bde70a4829e8f4e5
Mateu <mat30041986@gmail.com>
2013-07-04 14:53:58 +0100

    Removed automatic comment

--- src/Comparators/Comparator.php
+++ src/Comparators/Comparator.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Mateu Adsuara Sabater
- * Date: 4/07/13
- * Time: 11:05
- */
 
 interface Comparator {
     public function areEqual($a, $b);
