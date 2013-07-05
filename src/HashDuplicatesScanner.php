b722decf6522d565963dd5a7031307addc83c51d
Mateu <mat30041986@gmail.com>
2013-07-05 16:58:59 +0100

    HashDuplicatesScanner->getDuplicates() Working

--- src/HashDuplicatesScanner.php
+++ src/HashDuplicatesScanner.php
@@ -7,6 +7,7 @@ class HashDuplicatesScanner {
     private $reader;
 
     private $appearedRows, $uniqueRows = array();
+    private $duplicatedRows = array();
 
     function __construct(){
         $this->appearedRows = new HashList();
@@ -21,18 +22,23 @@ class HashDuplicatesScanner {
     }
 
     function getUniques(){
+        $this->processInput();
+        return array_values($this->uniqueRows);
+    }
+
+    private function processInput(){
         while(!$this->reader->isEof()){
             $row = $this->reader->readRow();
             $this->check($row);
         }
-        return array_values($this->uniqueRows);
     }
 
     private function check($row){
         $hash = $this->getHash($row);
 
         if ($this->appearedRows->contains($hash)) {
-            $this->removeNotUniqueRow($hash);
+            $this->moveUniqueToDuplicateRows($row, $hash);
+            $this->addDuplicateRow($row, $hash);
         } else {
             $this->addUniqueRow($row, $hash);
         }
@@ -46,12 +52,22 @@ class HashDuplicatesScanner {
         return $hash;
     }
 
-    private function removeNotUniqueRow($hash){
+    private function moveUniqueToDuplicateRows($row, $hash){
+        $this->addDuplicateRow($row, $hash);
         unset($this->uniqueRows[$hash]);
+    }
+
+    private function addDuplicateRow($row, $hash){
+        $this->duplicatedRows[$hash][] = $row;
     }
 
     private function addUniqueRow($row, $hash){
         $this->appearedRows->add($hash);
         $this->uniqueRows[$hash] = $row;
+    }
+
+    function getDuplicates(){
+        $this->processInput();
+        return array_values($this->duplicatedRows);
     }
 }
--- test/TestHashDuplicatesScanner.php
+++ test/TestHashDuplicatesScanner.php
@@ -74,6 +74,48 @@ class TestHashDuplicatesScanner extends TestFixture{
         );
         Assert::areIdentical($uniques, $scanner->getUniques());
     }
+
+    function testGettingDuplicatesWhenNoDuplicates(){
+        $dataOneColumnNoDuplicates = array(
+            array(
+                "Column1" => "Foo"
+            ),
+            array(
+                "Column1" => "Bar"
+            )
+        );
+
+        $scanner = $this->createScannerWithReader($dataOneColumnNoDuplicates);
+        Assert::areIdentical(array(), $scanner->getDuplicates());
+    }
+
+    function testGettingDuplicatesWhenDuplicates(){
+        $dataOneColumnWithDuplicates = array(
+            array(
+                "Column1" => "Foo"
+            ),
+            array(
+                "Column1" => "Bar"
+            ),
+            array(
+                "Column1" => "Bar"
+            )
+        );
+
+        $scanner = $this->createScannerWithReader($dataOneColumnWithDuplicates);
+
+        $duplicates = array(
+            array(
+                array(
+                    "Column1" => "Bar"
+                ),
+                array(
+                    "Column1" => "Bar"
+                )
+            )
+        );
+        Assert::areIdentical($duplicates, $scanner->getDuplicates());
+    }
 }
 
 class NotReadyMockReader implements \Reader{
