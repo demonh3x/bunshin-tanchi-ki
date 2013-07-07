87dea5bfc982ad327c0d3956f6a4b0d1540e4390
Mateu <mat30041986@gmail.com>
2013-07-07 20:40:57 +0100

    Exclude regex in the tests

--- test/runTests.php
+++ test/runTests.php
@@ -5,9 +5,13 @@ define("__ROOT_DIR__", "../");
 
 include_once('EnhanceTestFramework.php');
 $filterRegex =  "//";
+$excludeRegex = "//";
 
 foreach (glob("Test*.php") as $filename)
 {
+    if ($excludeRegex !== "//" && preg_match($excludeRegex, $filename)){
+        continue;
+    }
     if (preg_match($filterRegex, $filename)){
         include_once($filename);
     }
