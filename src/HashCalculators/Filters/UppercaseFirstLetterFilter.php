47fbb507f8e54cee8ed4c24dfa74c25a708f9cbb
Adrian Gonzalez <adriali8608@gmail.com>
2013-07-08 15:50:59 +0100

    UppercaseFirstLetterFilter done.

--- src/HashCalculators/Filters/UppercaseFirstLetterFilter.php
+++ src/HashCalculators/Filters/UppercaseFirstLetterFilter.php
@@ -4,6 +4,6 @@ include_once("Filter.php");
 
 class UppercaseFirstLetterFilter implements Filter{
     function applyTo($text){
-        return $text;
+        return ucwords(strtolower($text));
     }
 }
