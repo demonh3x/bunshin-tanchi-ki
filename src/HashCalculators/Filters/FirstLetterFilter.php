2d56fa2fced192f325ea971ce9494e36ad08ee0b
Adrian Gonzalez <adriali8608@gmail.com>
2013-07-08 16:02:46 +0100

    UppercaseFirstLetterFilter done.

--- src/HashCalculators/Filters/FirstLetterFilter.php
+++ src/HashCalculators/Filters/FirstLetterFilter.php
@@ -4,6 +4,11 @@ include_once("Filter.php");
 
 class FirstLetterFilter implements Filter{
     function applyTo($text){
+        if ($text != "")
+        {
+            $text = substr($text, 0, 1);
+        }
+
         return $text;
     }
 }
