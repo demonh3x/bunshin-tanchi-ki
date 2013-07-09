6624d2557f7aaae2965bf8e631dfef829485d600
Mateu <mat30041986@gmail.com>
2013-07-09 10:21:30 +0100

    Make only one instance of table data on OnlyLettersFilter

--- src/HashCalculators/Filters/OnlyLettersFilter.php
+++ src/HashCalculators/Filters/OnlyLettersFilter.php
@@ -3,17 +3,17 @@
 include_once("Filter.php");
 
 class OnlyLettersFilter implements Filter{
+    private static $letters = array("a","b","c","d","e","f","g","h","i","j","k","l",
+        "m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z",
+        "A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ",
+        "O","P","Q","R","S","T","U","V","W","X","Y","Z"," "
+    );
+
     function applyTo($text){
-        $letters = array("a","b","c","d","e","f","g","h","i","j","k","l",
-            "m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z",
-            "A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ",
-            "O","P","Q","R","S","T","U","V","W","X","Y","Z"," "
-        );
-        
         $onlyLettersText = "";
         for($i = 0; $i < strlen($text); $i++)
         {
-            foreach($letters as $letter){
+            foreach(static::$letters as $letter){
                 if ($text[$i] == $letter )
                 {
                     $onlyLettersText .= $text[$i];
