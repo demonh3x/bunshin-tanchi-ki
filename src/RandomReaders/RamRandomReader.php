51014075f8b2f414e75fb44719001fd285fe06b9
Mateu <mat30041986@gmail.com>
2013-07-09 22:22:40 +0100

    Tests and Implementation for RamRandomReader

---
+++ src/RandomReaders/RamRandomReader.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once("RandomReader.php");
+
+class RamRandomReader implements RandomReader{
+    private $pointerToGlobal;
+    private $ready = false;
+
+    function open($path){
+        $this->ready = isset($GLOBALS[$path]) && is_array($GLOBALS[$path]);
+        if ($this->isReady()){
+            $this->pointerToGlobal = &$GLOBALS[$path];
+        }
+    }
+
+    function isReady(){
+        return $this->ready;
+    }
+
+    function readRow($index){
+        return $this->pointerToGlobal[$index];
+    }
+
+    function getRowCount(){
+        return count($this->pointerToGlobal);
+    }
+}
---
+++ test/TestRamRandomReader.php
@@ -0,0 +1,88 @@
+<?php
+namespace Enhance;
+
+include_once(__ROOT_DIR__ . "src/RandomReaders/RamRandomReader.php");
+
+class TestRamReader extends TestFixture{
+
+    private $globalVariableName = "testGlobalVariable";
+
+    public function setUp(){
+    }
+
+    public function tearDown(){
+    }
+
+    private function createReader(){
+        return Core::getCodeCoverageWrapper("RamRandomReader");
+    }
+
+    function testOpeningNotDefinedGlobal(){
+        $reader = $this->createReader();
+
+        $reader->open("nonExistingGlobalVariable");
+        Assert::isFalse($reader->isReady());
+    }
+
+    function testOpeningNonArrayDefinedGlobal(){
+        $reader = $this->createReader();
+
+        $this->defineGlobalVariable("Foo");
+
+        $reader->open($this->globalVariableName);
+        $ready = $reader->isReady();
+
+        $this->undefineGlobalVariable();
+
+        Assert::isFalse($ready);
+    }
+
+    function testOpeningArrayDefinedGlobal(){
+        $reader = $this->createReader();
+
+        $this->defineGlobalVariable(array());
+
+        $reader->open($this->globalVariableName);
+        $ready = $reader->isReady();
+
+        $this->undefineGlobalVariable();
+
+        Assert::isTrue($ready);
+    }
+
+    private function defineGlobalVariable($value){
+        $GLOBALS[$this->globalVariableName] = $value;
+    }
+
+    private function undefineGlobalVariable(){
+        unset($GLOBALS[$this->globalVariableName]);
+    }
+
+    function testRowCount(){
+        $reader = $this->createReader();
+
+        $this->defineGlobalVariable(
+            array(
+                array("0" => "Foo"),
+                array("0" => "Bar")
+            )
+        );
+        $reader->open($this->globalVariableName);
+        Assert::areIdentical(2, $reader->getRowCount());
+    }
+
+    function testReadingRow(){
+        $reader = $this->createReader();
+
+        $this->defineGlobalVariable(
+            array(
+                array("0" => "Foo"),
+                array("0" => "Bar")
+            )
+        );
+        $reader->open($this->globalVariableName);
+        $expected = array("0" => "Bar");
+
+        Assert::areIdentical($expected, $reader->readRow(1));
+    }
+}
