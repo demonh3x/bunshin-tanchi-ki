d608c4906ab62260ea4f9aeeca064df6d19b117e
Adrian Gonzalez <adriali8608@gmail.com>
2013-07-09 12:44:36 +0100

    UppercaseFirstLetterFilter done.

--- src/Writers/CsvWriter.php
+++ src/Writers/CsvWriter.php
@@ -3,15 +3,25 @@
 include_once("Writer.php");
 
 class CsvWriter implements Writer{
+    private $ready = false,
+        $fp;
+
     function create($path) {
         // TODO: Implement create() method.
+        $this->fp = fopen($path.'file.csv', 'w');
+
     }
 
     function isReady() {
         // TODO: Implement isReady() method.
+
+        return $this->ready;
     }
 
     function writeRow($data) {
         // TODO: Implement writeRow() method.
+        foreach ($data as $fields) {
+            fputcsv($this->fp, $fields, ',', '"');
+        }
     }
 }
