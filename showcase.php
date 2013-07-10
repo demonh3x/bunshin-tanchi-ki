696252313114705264bd0afbb1f380c2cdb52547
Mateu <mat30041986@gmail.com>
2013-07-10 17:30:31 +0100

    Update Showcase with the new Algorithm

--- showcase.php
+++ showcase.php
@@ -11,18 +11,31 @@
 <body>
 <?php
     include_once("src/HashDuplicatesScanner.php");
-    include_once("src/Readers/CsvReader.php");
+    include_once("src/RandomReaders/CsvRandomReader.php");
     include_once("src/HashCalculators/StringHashCalculator.php");
     foreach (glob("src/HashCalculators/Filters/*.php") as $filename){
+        include_once($filename);
+    }
+    foreach (glob("src/Writers/*.php") as $filename){
+        include_once($filename);
+    }
+    foreach (glob("src/RandomReaders/*.php") as $filename){
         include_once($filename);
     }
 
     include_once("HTML.php");
 
+    $memoryUsage = memory_get_usage(true) / 1024 / 1024;
+    echo "<h1>Memory Usage: $memoryUsage MB</h1>";
+
+    $startTime = microtime(true);
+
     $scanner = new HashDuplicatesScanner();
 
-    $reader = new CsvReader();
-    $reader->open("test/sampleFiles/amaya_data_template.csv");
+    $reader = new CsvRandomReader();
+    /*$reader->open("test/sampleFiles/15000rows.csv");*/
+    $reader->open("test/sampleFiles/45000rows.csv");
+    /*$reader->open("test/sampleFiles/100000rows.csv");*/
     $scanner->setReader($reader);
 
     $calculator = new StringHashCalculator();
@@ -38,21 +51,56 @@
         new OnlyLettersFilter(),
         new UppercaseFirstLetterFilter()
     ), "4");
-
     $calculator->watchColumns(array("3", "4"));
-
     $scanner->setHashCalculator($calculator);
 
-    echo "<h1>Uniques</h1>";
-    $uniques = $scanner->getUniques();
-    echo HTML::table($uniques);
+    $uniquesWriter = new CsvWriter();
+    $uniquesWriter->create("showcase/uniques.csv");
+    $scanner->setUniquesWriter($uniquesWriter);
 
-    echo "<h1>Duplicates</h1>";
-    $duplicatesGroups = $scanner->getDuplicates();
-    foreach ($duplicatesGroups as $group){
-        echo HTML::table($group);
-        echo "<br>";
+
+    class CustomWriterFactory implements WriterFactory{
+        function createWriter($id){
+            $writer = new CsvWriter();
+            $writer->create("showcase/duplicates/$id.csv");
+            if (!$writer->isReady()){
+                throw new Exception("Writer not ready!");
+            }
+            return $writer;
+        }
     }
+    $scanner->setDuplicatesWriterFactory(new CustomWriterFactory());
+
+    $memoryUsage = memory_get_usage(true) / 1024 / 1024;
+    echo "<h1>Memory Usage: $memoryUsage MB</h1>";
+
+    $scanner->scan();
+
+    echo "<h1>Done!</h1>";
+/*
+    function getRamArray($ramId){
+        $reader = new RamRandomReader();
+        $reader->open($ramId);
+        $ret = array();
+        for ($i = 0; $i < $reader->getRowCount(); $i++){
+            $row = $reader->readRow($i);
+            $ret[] = $row;
+
+        }
+        return $ret;
+    }
+    echo "<h1>Uniques</h1>";
+    echo HTML::table(getRamArray("ShowcaseUniques"));*/
+
+
+    $endTime = microtime(true);
+    $executionTime = $endTime - $startTime;
+
+    $memoryUsage = memory_get_usage(true) / 1024 / 1024;
+
+    echo "<h1>Execution Time: $executionTime seconds</h1>";
+    echo "<h1>Memory Usage: $memoryUsage MB</h1>";
+
 ?>
 </body>
 </html>
