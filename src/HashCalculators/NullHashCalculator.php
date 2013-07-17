901a70470620fad5fd362ca369cae6cb3d3e0dce
Mateu <mat30041986@gmail.com>
2013-07-17 12:50:00 +0100

    NullHashCalculator works with arrays

--- src/HashCalculators/NullHashCalculator.php
+++ src/HashCalculators/NullHashCalculator.php
@@ -2,6 +2,9 @@
 
 class NullHashCalculator implements HashCalculator{
     function calculate($data){
+        if (is_array($data)){
+            return  hash('md4',serialize($data));
+        }
         return spl_object_hash($data);
     }
 }
