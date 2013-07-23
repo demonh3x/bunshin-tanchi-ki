2f68060dadf129cc2b7490fbad121f1d68f8603f
Mateu <mat30041986@gmail.com>
2013-07-23 17:18:21 +0100

    Tests and implementation for OccurrenceScanner

--- src/OccurrenceScanner.php
+++ src/OccurrenceScanner.php
@@ -1,11 +1,61 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Mateu Adsuara Sabater
- * Date: 23/07/13
- * Time: 10:24
- */
+
+include_once("Row.php");
+include_once("RowCollection.php");
+
+include_once("HashCalculators/RowFilter.php");
 
 class OccurrenceScanner {
+    protected $reader, $regex, $columnsToScan, $rowFilter;
+    protected $rowList = array();
 
+    function __construct(RandomReader $reader, $regex, $columns = array(), RowFilter $rowFilter = null){
+        $this->reader = $reader;
+        $this->regex = $regex;
+        $this->columnsToScan = $columns;
+        $this->rowFilter = is_null($rowFilter)? new RowFilter(): $rowFilter;
+    }
+
+    function getResults(){
+        $this->processAllRows();
+        return $this->getResultsList();
+    }
+
+    protected function processAllRows(){
+        for ($rowIndex = 0; $rowIndex < $this->reader->getRowCount(); $rowIndex++) {
+            $row = $this->readRow($rowIndex);
+            $this->processRow($row);
+        }
+    }
+
+    protected function processRow(Row $row){
+        $filteredData = $this->rowFilter->applyTo($row->getData());
+
+        $rowColumns = array_keys($filteredData);
+        $columns = $this->areColumnsDefined()? $this->columnsToScan : $rowColumns;
+
+        foreach ($columns as $column) {
+            $value = &$filteredData[$column];
+            if (preg_match($this->regex, $value)) {
+                $this->addOccurrence($row);
+                return;
+            }
+        }
+    }
+
+    protected function areColumnsDefined(){
+        return !empty($this->columnsToScan);
+    }
+
+    protected function readRow($rowIndex){
+        return new Row($this->reader, $rowIndex);
+    }
+
+    protected function addOccurrence(Row $row){
+        $this->rowList[] = $row;
+    }
+
+    protected function getResultsList(){
+        return new RowCollection($this->rowList);
+    }
 }
--- test/TestOccurrenceScanner.php
+++ test/TestOccurrenceScanner.php
@@ -1,7 +1,13 @@
 <?php
 namespace Enhance;
 
+include_once(__ROOT_DIR__ . "src/OccurrenceScanner.php");
 
+include_once(__ROOT_DIR__ . "src/Writers/RamWriter.php");
+include_once(__ROOT_DIR__ . "src/RandomReaders/RamRandomReader.php");
+
+include_once(__ROOT_DIR__ . "src/HashCalculators/RowFilter.php");
+include_once("mocks/LowercaseMockFilter.php");
 
 class TestOccurrenceScanner extends TestFixture{
 
@@ -11,9 +17,111 @@ class TestOccurrenceScanner extends TestFixture{
     public function tearDown(){
     }
 
-    function test(){
-        Assert::fail();
+    private function createScanner($data, $regex, $columns = array(), $rowFilter = null){
+        $reader = $this->createRamReader("testOccurrenceScannerDefaultRamReader", $data);
+        $scanner = new \OccurrenceScanner($reader, $regex, $columns, $rowFilter);
+
+        return $scanner;
     }
 
+    private function createRamReader($id, $data){
+        $writer = new \RamWriter();
 
+        unset($GLOBALS[$id]);
+        $writer->create($id);
+
+        foreach ($data as $row){
+            $writer->writeRow($row);
+        }
+
+        $reader = new \RamRandomReader();
+        $reader->open($id);
+
+        return $reader;
+    }
+
+    function testGetResultsReturnsAnInstanceOfIterator(){
+        $scanner = $this->createScanner(array(), "//");
+        Assert::isTrue($scanner->getResults() instanceof \Iterator);
+    }
+
+    private function assertOccurrences($input, $expected, $regex, $columns = array(), $rowFilter = null){
+        $scanner = $this->createScanner($input, $regex, $columns, $rowFilter);
+
+        $output = $scanner->getResults();
+        $outputArray = array();
+        foreach ($output as $row){
+            $outputArray[] = $row;
+        }
+
+        Assert::areIdentical($expected, $outputArray);
+    }
+
+    function testNoOccurrences(){
+        $input = array(
+            array("0" => "Hello!"),
+            array("0" => "World!"),
+        );
+        $regex = "/^Foo/";
+        $expected = array();
+
+        $this->assertOccurrences($input, $expected, $regex);
+    }
+
+    function testAllOccurrences(){
+        $input = array(
+            array("0" => "Foo!"),
+            array("0" => "Fooaaaa")
+        );
+        $regex = "/^Foo/";
+        $expected = array(
+            array("0" => "Foo!"),
+            array("0" => "Fooaaaa")
+        );
+
+        $this->assertOccurrences($input, $expected, $regex);
+    }
+
+    function testTwoColumnsMatching(){
+        $input = array(
+            array("0" => "Foo!", "1" => "Foooooo"),
+            array("0" => "Bar", "1" => "Bar")
+        );
+        $regex = "/^Foo/";
+        $expected = array(
+            array("0" => "Foo!", "1" => "Foooooo")
+        );
+
+        $this->assertOccurrences($input, $expected, $regex);
+    }
+
+    function testSearchInOneColumn(){
+        $input = array(
+            array("0" => "Foo", "1" => "Bar"),
+            array("0" => "Bar", "1" => "Foo")
+        );
+        $expected = array(
+            array("0" => "Foo", "1" => "Bar")
+        );
+        $regex = "/^Foo/";
+        $columns = array("0");
+
+        $this->assertOccurrences($input, $expected, $regex, $columns);
+    }
+
+    function testFilteredMatching(){
+        $input = array(
+            array("0" => "Foo"),
+            array("0" => "Bar")
+        );
+        $regex = "/^foo/";
+        $expected = array(
+            array("0" => "Foo")
+        );
+
+        $rowFilter = new \RowFilter();
+        $rowFilter->setGlobalFilter(new LowercaseMockFilter());
+
+        $this->assertOccurrences($input, $expected, $regex, array(), $rowFilter);
+    }
 }
