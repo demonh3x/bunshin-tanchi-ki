ac69bfb55f245fd26c13cf3cc303a83066fa7d38
Adrian Gonzalez <adriali8608@gmail.com>
2013-07-30 16:17:06 +0100

    Generate Fields links in duplicates groups using the UniquePURLGenerator     - create generated file in the same folder as original     - move original file to merged folder
     Added Number of Rows next to each file.

--- ui/generateFields.php
+++ ui/generateFields.php
@@ -7,11 +7,13 @@
     <?php
         include_once("common.php");
         include_once(__ROOT_DIR__ . "src/Writers/CsvWriter.php");
+        include_once(__ROOT_DIR__ . "src/CellGenerators/UniquePURLGenerator.php");
         foreach (glob(__ROOT_DIR__ . "src/HashCalculators/Filters/*.php") as $filename){
             include_once($filename);
         }
 
         $file = $_REQUEST["file"];
+        $deduplicationsWorkFolder = getDeduplicationsWorkFolder($file);
         /*$path = getPath($file);*/
 
         function getPath($filePath){
@@ -24,7 +26,10 @@
             $inputFileName = $pathParts[count($pathParts) -1];
 
             $inputFileNameWOExtension = explode(".", $inputFileName, 2)[0];
-            $outputFile = $inputFileNameWOExtension . ".fg$index.csv";
+            $pathParts[count($pathParts)-1] = $inputFileNameWOExtension . ".fg$index.csv";
+
+            $outputFile = implode("/", $pathParts);
+
 
             return $outputFile;
         }
@@ -44,6 +49,36 @@
 
             return $outputFile;
         }
+
+        function getDeduplicationsWorkFolder($file) {
+            $pathParts = explode("/", $file);
+            $deduplicationsWorkFolder = $pathParts[0] . "/" . $pathParts[1] . "/" . $pathParts[2] . "/";
+            return $deduplicationsWorkFolder;
+        }
+
+        function getExistingPURLsPath ($deduplicationsWorkFolder) {
+            $existingPurlsFile = $deduplicationsWorkFolder . __IDENTIFYING_VALUES_FILE__;
+            return $existingPurlsFile;
+        }
+
+        function getBeforeGeneratingDuplicatesFolder ($deduplicationsWorkFolder) {
+            $beforeGeneratingDuplicatesFolder = $deduplicationsWorkFolder . __DUPLICATES_FOLDER__ . __BEFORE_GENERATING_FOLDER__;
+            if (!is_dir($beforeGeneratingDuplicatesFolder)){
+                mkdir($beforeGeneratingDuplicatesFolder);
+            }
+            return $beforeGeneratingDuplicatesFolder;
+        }
+
+        function getArrayOfExistingPURLs($file) {
+            $reader = new CsvRandomReader();
+            $reader->open($file);
+
+            for ($i = 0; $i < $reader->getRowCount(); $i++){
+                $arrayUsedPurls[] = $reader->readRow($i)[0];
+            }
+
+            return $arrayUsedPurls;
+        }
     ?>
 
 
@@ -58,23 +93,28 @@
         $purlColumn = "7";
         $firstNameColumn = "3";
         $lastNameColumn = "4";
+        $salutationColumn = "2";
+
+        $existingPurlsFile = getExistingPURLsPath($deduplicationsWorkFolder);
+        $arrayUsedPurls = getArrayOfExistingPURLs($existingPurlsFile);
+
+        $uniquePURLGenerator = new UniquePURLGenerator($firstNameColumn, $lastNameColumn, $salutationColumn, $purlColumn, $arrayUsedPurls);
+
         for ($i = 0; $i < $reader->getRowCount(); $i++){
             $row = $reader->readRow($i);
-
-            $filters = FilterGroup::create(
-                new TrimFilter(),
-                new CutFromFirstSpaceFilter(),
-                new UppercaseFirstLetterFilter()
-            );
-            $firsName = $filters->applyTo($row[$firstNameColumn]);
-            $lastName = $filters->applyTo($row[$lastNameColumn]);
-
-            $row[$purlColumn] = $firsName . $lastName;
-
-            $writer->writeRow($row);
+            $writer->writeRow($uniquePURLGenerator->generate($row));
         }
+
+        $pathParts = explode("/", $file);
+        $inputFileName = $pathParts[count($pathParts) -1];
+
+        $beforeGeneratingDuplicatesFolder = getBeforeGeneratingDuplicatesFolder($deduplicationsWorkFolder) . $inputFileName;
+
+        rename($file, $beforeGeneratingDuplicatesFolder);
     ?>
 
     <h1>Generate fields from file: <?= $file ?> to <?= $outputFile ?></h1>
+    <hr>
+    <h1>Moving: <?= $file ?> to <?=$beforeGeneratingDuplicatesFolder ?></h1>
 </body>
 </html>
