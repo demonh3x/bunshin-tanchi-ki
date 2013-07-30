106dee0b46010d6c8ea55d1de852118578f1d6a4
Mateu <mat30041986@gmail.com>
2013-07-30 11:59:01 +0100

    UniquePURLGenerator cleans surname when generating

--- src/CellGenerators/UniquePURLGenerator.php
+++ src/CellGenerators/UniquePURLGenerator.php
@@ -4,10 +4,16 @@ include_once(__ROOT_DIR__ . "src/HashList.php");
 foreach (glob(__ROOT_DIR__ . "src/CellGenerators/PurlCalculators/*.php") as $filename){
     include_once($filename);
 }
+include_once(__ROOT_DIR__ . "src/HashCalculators/RowFilter.php");
+include_once(__ROOT_DIR__ . "src/HashCalculators/Filters/FilterGroup.php");
+include_once(__ROOT_DIR__ . "src/HashCalculators/Filters/SubstituteAccentsFilter.php");
+include_once(__ROOT_DIR__ . "src/HashCalculators/Filters/OnlyLettersFilter.php");
+include_once(__ROOT_DIR__ . "src/HashCalculators/Filters/NoSpacesFilter.php");
 
 class UniquePURLGenerator {
     private $purlField;
     private $hashList;
+    private $rowFilter;
 
     private $purlCalculators = array();
 
@@ -18,6 +24,16 @@ class UniquePURLGenerator {
         foreach ($usedPurls as $purl){
             $this->hashList->add($purl);
         }
+
+        $this->rowFilter = new RowFilter();
+        $this->rowFilter->setFilter(
+            FilterGroup::create(
+                new SubstituteAccentsFilter(),
+                new OnlyLettersFilter(),
+                new NoSpacesFilter()
+            ),
+            $surnameField
+        );
 
         $this->purlCalculators[] = new NameSurnameCalculator($firstnameField, $surnameField, $salutationField);
         $this->purlCalculators[] = new NameSCalculator($firstnameField, $surnameField, $salutationField);
@@ -32,7 +48,9 @@ class UniquePURLGenerator {
         $purlHasBeenGenerated = false;
 
         foreach ($this->purlCalculators as $calculator){
-            $purl = $calculator->calculate($row);
+            $purl = $calculator->calculate(
+                $this->rowFilter->applyTo($row)
+            );
 
             if (!$this->hashList->contains($purl)){
                 $return[$this->purlField] = $purl;
