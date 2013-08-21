c4c4de488e62d237dc62d06b68424da64f8396ba
Mateu <mat30041986@gmail.com>
2013-08-21 23:31:51 +0100

    Refactor HashUniquesExporter to use the new RowListener classes

--- src/HashUniquesExporter.php
+++ src/HashUniquesExporter.php
@@ -8,6 +8,10 @@ include_once("Writers/NullWriterFactory.php");
 include_once("HashCalculators/RowFilter.php");
 include_once("HashCalculators/NullRowFilter.php");
 
+include_once("RowListeners/ExportingRowListener.php");
+include_once("Writers/FilteringWriter.php");
+include_once("Writers/FilteringWriterFactory.php");
+include_once("RowListeners/HashGroupExportingRowListener.php");
 class HashUniquesExporter extends HashUniquesScanner{
     function __construct(HashCalculator $hashCalculator, UniquesList $uniquesList, $randomReaders = array()){
         parent::__construct($hashCalculator, $uniquesList, $randomReaders);
@@ -21,42 +25,13 @@ class HashUniquesExporter extends HashUniquesScanner{
             $cleanerFilter = new NullRowFilter();
         }
 
-        $duplicatesListener = new DuplicatesExporter(
-            $duplicatesFactory,
-            $cleanerFilter
+        $this->scan(
+            new ExportingRowListener(
+                new FilteringWriter($uniquesWriter, $cleanerFilter)
+            ),
+            new HashGroupExportingRowListener(
+                new FilteringWriterFactory($duplicatesFactory, $cleanerFilter)
+            )
         );
-
-        $uniqueRows = $this->scan($duplicatesListener);
-        foreach ($uniqueRows as $uniqueRow){
-            $uniquesWriter->writeRow(
-                $cleanerFilter->applyTo($uniqueRow)
-            );
-        }
-    }
-}
-
-include_once("Writers/WriterFactory.php");
-class DuplicatesExporter implements RowListener{
-    private $factory;
-    private $writers = array();
-
-    private $rowFilter;
-
-    function __construct(WriterFactory $factory, RowFilter $rowFilter){
-        $this->factory = $factory;
-        $this->rowFilter = $rowFilter;
-    }
-
-    function receiveRow(RandomReader $reader, $rowIndex, $rowHash){
-        $filteredRow = $this->rowFilter->applyTo($reader->readRow($rowIndex));
-        $writer = $this->getWriter($rowHash);
-        $writer->writeRow($filteredRow);
-    }
-
-    private function getWriter($hash){
-        if (!isset($this->writers[$hash])) {
-            $this->writers[$hash] = $this->factory->createWriter($hash);
-        }
-        return $this->writers[$hash];
     }
 }
