f0da40c09e12715b1784b9ae1e29877b3fd98e50
Mateu <mat30041986@gmail.com>
2013-08-22 17:00:27 +0100

    GroupsExportingRowListener childs only have to implement getGroupId() method

--- src/RowListeners/HashGroupsExportingRowListener.php
+++ src/RowListeners/HashGroupsExportingRowListener.php
@@ -4,8 +4,7 @@ include_once("RowListener.php");
 include_once("GroupsExportingRowListener.php");
 
 class HashGroupsExportingRowListener extends GroupsExportingRowListener implements RowListener {
-    function receiveRow(RandomReader $reader, $rowIndex, $rowHash){
-        $writer = $this->getWriter($rowHash);
-        $writer->writeRow($reader->readRow($rowIndex));
+    protected function getGroupId(RandomReader $reader, $rowIndex, $rowHash){
+        return $rowHash;
     }
 }
