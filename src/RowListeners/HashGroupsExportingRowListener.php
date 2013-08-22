1e3c170feac50b3cf7a02daa8ff64787688a4f3e
Mateu <mat30041986@gmail.com>
2013-08-22 14:20:50 +0100

    Removed unnecessary imports

--- src/RowListeners/HashGroupsExportingRowListener.php
+++ src/RowListeners/HashGroupsExportingRowListener.php
@@ -2,9 +2,6 @@
 
 include_once("RowListener.php");
 include_once("GroupsExportingRowListener.php");
-include_once(__ROOT_DIR__ . "src/Writers/WriterFactory.php");
-include_once(__ROOT_DIR__ . "src/HashCalculators/RowFilter.php");
-include_once(__ROOT_DIR__ . "src/HashCalculators/NullRowFilter.php");
 
 class HashGroupsExportingRowListener extends GroupsExportingRowListener implements RowListener {
     function receiveRow(RandomReader $reader, $rowIndex, $rowHash){
